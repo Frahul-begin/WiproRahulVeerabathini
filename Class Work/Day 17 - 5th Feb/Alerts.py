ac7bfd68f1b7f38d33a124a133f29a60ca03021f
Rahul Veerabathini <rahulveerabathini98@gmail.com>
2026-02-05 17:00:48 +0530

    Add files via upload

---
+++ Class Work/Day 17 - 5th Feb/Alerts.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+from selenium.webdriver.support.ui import Select
+driver = webdriver.Firefox()
+driver.get("https://letcode.in/alert")
+driver.find_element(By.ID, "accept").click()
+alert=driver.switch_to.alert
+print(alert.text)
+alert.accept()
+
+
+driver.find_element(By.ID, "confirm").click()
+alert=driver.switch_to.alert
+print(alert.text)
+alert.accept()
+
+driver.find_element(By.ID, "prompt").click()
+alert=driver.switch_to.alert
+print(alert.text)
+alert.accept()
+driver.quit()
